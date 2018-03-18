9ba2e3be8cc3839bec402ca05eec5a06f646e5cb
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 01:20:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizzbuzz(num)
   d_3 = num / 3
   d_5 = num / 5
   
-  c_3 = d_3 == d_3.floor
-  c_5 = d_5 == d_5.floor
+  c_3 = (d_3 == d_3.floor)
+  c_5 = (d_5 == d_5.floor)
   
   if true && c_3 && c_5
     return "FizzBuzz"
